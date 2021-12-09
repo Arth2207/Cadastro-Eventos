30d5e44cf8ee94b1edda97e0ca4aaa5fed773fcf
Arth2207 <94029700+Arth2207@users.noreply.github.com>
2021-12-09 19:53:24 -0300

    commit qualquer

---
+++ cadastroEventos.js
@@ -0,0 +1,19 @@
+let dataEvento = 21/12/2021;
+    let dataAtual  = 9/12/2021;
+    if(dataEvento<=dataAtual){
+        console.log("A data do evento nao pode ser anterior e nem igual a data atual")
+    } else{
+        console.log("data permitida!")
+    } 
+    var listaDeParticipantes =["Maria","João","Pedro","Gustavo", "gabriel"]
+    if(listaDeParticipantes.length > 100){
+        console.log("o evento não poderá exceder 100 participantes")
+    } else{
+        console.log("Evento criado com sucesso")
+    } 
+    let idadedoParticipante = 17;
+    if(idadedoParticipante >= 18) {
+        console.log("Cadastro permitido")
+    } else{
+        console.log(" Cadastro negado, o criador do evento deverá ter ao menos 18 anos")
+    }
